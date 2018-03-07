01fb8d19f1584f3650d094c18140e7308659385d
Alan <31071359+amechu@users.noreply.github.com>
2018-03-07 13:53:46 -0300

    Agregue cosas

--- TP2EDA/TP2EDA/parseCmd.h
+++ TP2EDA/TP2EDA/parseCmd.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+//DEFINICIONES
+#define NPARAMETERS 3
+#define NKEYS 3
+#define NVALUES 3 //Puede cambiarse el numero para la cantidad de parametros, claves y valores que se deseen
+enum { ERROR1 = -3, ERROR2, ERROR3 }; //error 1 es opcion sin valor, error 2 es opcion sin clave error3 es no encontrado
+
+//DECLARACIONES
+int parseCallback(char *key, char *value, void *userData);
+int parseCmdLine(int argc, char *argv[], pCallback p, void *userData);
+
+//TYPEDEF
+typedef int(*pCallback) (char *, char*, void *);
+typedef struct
+{
+	char *  parameters[NPARAMETERS];
+	char *  key[NKEYS];
+	char *  value[NVALUES];
+}userData;
